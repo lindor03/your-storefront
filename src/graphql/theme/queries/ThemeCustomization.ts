c45a9c664ab4ebeb3b1dfee27ade4cf3eba4c2f0
lindormorina <lindormorina4@gmail.com>
2026-01-12 15:36:00 -0800

    aaa

--- src/graphql/theme/queries/ThemeCustomization.ts
+++ src/graphql/theme/queries/ThemeCustomization.ts
@@ -4,15 +4,60 @@ export const GET_THEME_CUSTOMIZATION = gql`
   query ThemeCustomization {
     themeCustomization {
       id
+      themeCode
       type
       name
+      sortOrder
       status
+      channelId
+      createdAt
+      updatedAt
       translations {
         id
+        themeCustomizationId
+        localeCode
         options {
           title
           css
           html
+
+          links {
+            url
+            slug
+            type
+            id
+          }
+
+          images {
+            link
+            image
+            title
+            imageUrl
+          }
+
+          column_1 {
+            url
+            title
+            sortOrder
+          }
+
+          column_2 {
+            url
+            title
+            sortOrder
+          }
+
+          column_3 {
+            url
+            title
+            sortOrder
+          }
+
+          services {
+            title
+            description
+            serviceIcon
+          }
         }
       }
     }
