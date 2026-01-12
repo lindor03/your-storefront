826b4b8c5f451ca64c70171108dc1bcd3551fc28
lindormorina <lindormorina4@gmail.com>
2026-01-12 15:38:31 -0800

    kjhk

--- src/graphql/catelog/queries/Category.ts
+++ src/graphql/catelog/queries/Category.ts
@@ -1,44 +1,22 @@
 import { gql } from "@apollo/client";
 
 export const GET_TREE_CATEGORIES = gql`
-  query TreeCategories($parentId: Int) {
-    homeCategories(
-      getCategoryTree: true
-      input: [
-        { key: "status", value: "1" }
-        { key: "parent_id", value: $parentId }
-      ]
-    ) {
+  query treeCategories($parentId: String) {
+    treeCategories(parentId: $parentId) {
       id
       position
       logoPath
-      logoUrl
       status
-      displayMode
-      parentId
-      name
-      slug
-      urlPath
-      description
-      metaTitle
-      metaDescription
-      metaKeywords
-
-      children {
-        id
-        position
-        logoPath
-        logoUrl
-        status
-        displayMode
-        parentId
-        name
-        slug
-        urlPath
-        description
-        metaTitle
-        metaDescription
-        metaKeywords
+      translations {
+        edges {
+          node {
+            name
+            slug
+            urlPath
+            description
+            metaTitle
+          }
+        }
       }
     }
   }
